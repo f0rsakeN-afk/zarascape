f99e2a0b1590abe2741477559e9828c48c01dc15
f0rsakeN-afk <nar3sh0@gmail.com>
2025-01-13 19:34:06 +0545

    image editor code done but has small bug left to fix

---
+++ src/components/ImageEditor.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import FilerobotImageEditor, { TABS, TOOLS } from 'react-filerobot-image-editor';
+import { createEditorConfig } from '../config/editorConfig';
+
+interface ImageEditorProps {
+    imageUrl: string;
+    onClose: () => void;
+    onSave: (editedImageUrl: string) => void;
+}
+
+export const ImageEditor: React.FC<ImageEditorProps> = ({
+    imageUrl,
+    onClose,
+    onSave,
+}) => {
+    const config = createEditorConfig(imageUrl);
+
+    return (
+        <div className="flex flex-col w-full h-full min-h-[100dvh] overflow-auto overscroll-none">
+            <FilerobotImageEditor
+                className="flex-1"
+                {...config}
+                onSave={(el) => {
+                    onSave(el.imageBase64);
+                }}
+                onClose={onClose}
+                defaultTabId={TABS.ADJUST}
+                defaultToolId={TOOLS.CROP}
+                savingPixelRatio={2}
+                previewPixelRatio={2}
+            />
+        </div>
+    );
+};
